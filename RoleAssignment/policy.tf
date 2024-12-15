aa8f8071f38c3424412484e4291861ec15ca3e8f
Pedro Zuzi <pedropz0750@gmail.com>
2024-12-15 23:59:05 +0000

    updare role

--- RoleAssignment/policy.tf
+++ RoleAssignment/policy.tf
@@ -14,9 +14,16 @@ resource "azurerm_policy_definition" "storage_account_role_assignment" {
 
 }
 
+data "azurerm_role_definition" "uaa" {
+  name = "User Access Administrator"
+}
+
+data "azurerm_subscription" "primary" {}
+
+
 resource "azurerm_role_assignment" "roleAssignement" {
   scope                = "/subscriptions/52c93eeb-4017-4b73-9816-d8b8a92aff1e"
-  role_definition_name = "User Access Administrator"
+  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.uaa.id}"
   principal_id         = azurerm_subscription_policy_assignment.tag_based_role_assignment.identity[0].principal_id
 
   depends_on = [azurerm_policy_definition.storage_account_role_assignment]
