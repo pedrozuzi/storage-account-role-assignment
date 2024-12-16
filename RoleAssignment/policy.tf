614b5f754db1ff0b397dab2d02c657f0213f0dae
Pedro Zuzi <pedropz0750@gmail.com>
2024-12-16 17:44:04 +0000

    change owner to user access admin and contibutor

--- RoleAssignment/policy.tf
+++ RoleAssignment/policy.tf
@@ -15,15 +15,27 @@ resource "azurerm_policy_definition" "storage_account_role_assignment" {
 }
 
 data "azurerm_role_definition" "uaa" {
-  name = "Owner"
+  name = "User Access Administrator"
+}
+
+data "azurerm_role_definition" "contributor" {
+  name = "Contributor"
 }
 
 data "azurerm_subscription" "primary" {}
 
 
-resource "azurerm_role_assignment" "roleAssignement" {
+resource "azurerm_role_assignment" "roleAssignement_uaa" {
   scope                = "/subscriptions/52c93eeb-4017-4b73-9816-d8b8a92aff1e"
   role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.uaa.id}"
+  principal_id         = azurerm_subscription_policy_assignment.tag_based_role_assignment.identity[0].principal_id
+
+  depends_on = [azurerm_policy_definition.storage_account_role_assignment]
+}
+
+resource "azurerm_role_assignment" "roleAssignement_contributor" {
+  scope                = "/subscriptions/52c93eeb-4017-4b73-9816-d8b8a92aff1e"
+  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
   principal_id         = azurerm_subscription_policy_assignment.tag_based_role_assignment.identity[0].principal_id
 
   depends_on = [azurerm_policy_definition.storage_account_role_assignment]
@@ -37,12 +49,4 @@ resource "azurerm_subscription_policy_assignment" "tag_based_role_assignment" {
   identity {
     type = "SystemAssigned" //the assignment will need to have an identity (system or used managed) which can be used to assign the User Access Administrator RBAC role. 
   }
-}
-
-resource "azurerm_subscription_policy_remediation" "rbac_remediation" {
-  name = "tag-based-role-assignment"
-  subscription_id = "/subscriptions/52c93eeb-4017-4b73-9816-d8b8a92aff1e" //this is your subscription id
-  policy_assignment_id =  azurerm_subscription_policy_assignment.tag_based_role_assignment.id
-
-  depends_on = [azurerm_role_assignment.roleAssignement]
 }
